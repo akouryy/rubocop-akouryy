7d06679f804b291a8b5ab12f29dfabb19bb56e37
akouryy <akouryy7@gmail.com>
2018-08-30 20:46:37 +0900

    Add specs when method call is inside parens

--- spec/rubocop/cop/akouryy/redundant_parentheses_for_method_call_spec.rb
+++ spec/rubocop/cop/akouryy/redundant_parentheses_for_method_call_spec.rb
@@ -67,88 +67,115 @@ describe RuboCop::Cop::Akouryy::RedundantParenthesesForMethodCall, :config do
       RUBY
     end
 
-    it 'accepts parens for method calls that are operands of unary operators' do
-      expect_no_offenses <<~RUBY
-        +foo(0)
-        -foo(0)
-        !foo(0)
-      RUBY
+    context 'when method call is not inside parens' do
+      it 'accepts parens for method calls that are operands of unary operators' do
+        expect_no_offenses <<~RUBY
+          +foo(0)
+          -foo(0)
+          !foo(0)
+        RUBY
+      end
+
+      it 'accepts parens for method calls prefixed by splat-like syntaxes' do
+        expect_no_offenses <<~RUBY
+          foo *bar(0)
+          foo **bar(0)
+          foo &bar(0)
+          foo = *bar(0)
+        RUBY
+      end
+
+      it 'accepts parens for method calls that are operands of' \
+          ' binary operators with high precedence' do
+        expect_no_offenses <<~RUBY
+          1 + foo(0)
+          foo(0) ** 2
+          foo(0) && 3
+          4 || foo(0)
+          foo(0)..5
+          6...foo(0)
+        RUBY
+      end
+
+      it 'accepts parens for method calls followed by dot' do
+        expect_no_offenses <<~RUBY
+          foo(0).bar
+        RUBY
+      end
+
+      it 'accepts parens for method calls with argument(s) followed by braces' do
+        expect_no_offenses <<~RUBY
+          foo(0){}
+        RUBY
+      end
+
+      it 'accepts parens for method calls with argument(s) followed by brackets' do
+        expect_no_offenses <<~RUBY
+          foo(0)[1, 2]
+          foo(0)[1, 2] = 3
+        RUBY
+      end
+
+      it 'accepts parens for special call syntax of method `call`' do
+        expect_no_offenses <<~RUBY
+          foo.(0)
+        RUBY
+      end
+
+      it 'accepts parens for method calls that are non-final args of other method calls' do
+        expect_no_offenses <<~RUBY
+          foo bar(0), 1
+          foo 0, bar(1), 2
+          foo 0, bar(1), *baz
+        RUBY
+      end
+
+      it 'accepts parens for method calls in array brackets or hash braces' do
+        expect_no_offenses <<~RUBY
+          [foo(0)]
+          { foo: bar(0) }
+          foo bar: baz(0)
+        RUBY
+      end
+
+      it 'accepts parens for method calls in when condition' do
+        expect_no_offenses <<~RUBY
+          case 0; when foo(1); end
+        RUBY
+      end
+
+      it 'accepts redundant parens which is not part of method calls' do
+        expect_no_offenses <<~RUBY
+          foo (0)
+          foo (0 + 1)
+          foo (bar 0), 1
+        RUBY
+      end
     end
 
-    it 'accepts parens for method calls prefixed by splat-like syntaxes' do
-      expect_no_offenses <<~RUBY
-        foo *bar(0)
-        foo **bar(0)
-        foo &bar(0)
-        foo = *bar(0)
-      RUBY
-    end
-
-    it 'accepts parens for method calls that are operands of' \
-        ' binary operators with high precedence' do
-      expect_no_offenses <<~RUBY
-        1 + foo(0)
-        foo(0) ** 2
-        foo(0) && 3
-        4 || foo(0)
-        foo(0)..5
-        6...foo(0)
-      RUBY
-    end
-
-    it 'accepts parens for method calls followed by dot' do
-      expect_no_offenses <<~RUBY
-        foo(0).bar
-      RUBY
-    end
-
-    it 'accepts parens for method calls with argument(s) followed by braces' do
-      expect_no_offenses <<~RUBY
-        foo(0){}
-      RUBY
-    end
-
-    it 'accepts parens for method calls with argument(s) followed by brackets' do
-      expect_no_offenses <<~RUBY
-        foo(0)[1, 2]
-        foo(0)[1, 2] = 3
-      RUBY
-    end
-
-    it 'accepts parens for special call syntax of method `call`' do
-      expect_no_offenses <<~RUBY
-        foo.(0)
-      RUBY
-    end
-
-    it 'accepts parens for method calls that are non-final args of other method calls' do
-      expect_no_offenses <<~RUBY
-        foo bar(0), 1
-        foo 0, bar(1), 2
-        foo 0, bar(1), *baz
-      RUBY
-    end
-
-    it 'accepts parens for method calls in array brackets or hash braces' do
-      expect_no_offenses <<~RUBY
-        [foo(0)]
-        { foo: bar(0) }
-        foo bar: baz(0)
-      RUBY
-    end
-
-    it 'accepts parens for method calls in when condition' do
-      expect_no_offenses <<~RUBY
-        case 0; when foo(1); end
-      RUBY
-    end
-
-    it 'accepts redundant parens which is not part of method calls' do
-      expect_no_offenses <<~RUBY
-        foo (0)
-        foo (0 + 1)
-        foo (bar 0), 1
-      RUBY
+    context 'when method call is inside parens' do
+      it 'registers an offense for parens of method call' do
+        expect_offense <<~RUBY
+          +(foo(0))
+               ^ Do not use unnecessary parentheses for method calls.
+          foo *(bar(0))
+                   ^ Do not use unnecessary parentheses for method calls.
+          1 + (foo(0))
+                  ^ Do not use unnecessary parentheses for method calls.
+          (foo(0)).bar
+              ^ Do not use unnecessary parentheses for method calls.
+          (foo(0))[1, 2]
+              ^ Do not use unnecessary parentheses for method calls.
+          foo (bar(0)), 1
+                  ^ Do not use unnecessary parentheses for method calls.
+          foo 0, (bar(1)), 2
+                     ^ Do not use unnecessary parentheses for method calls.
+          [(foo(0))]
+               ^ Do not use unnecessary parentheses for method calls.
+          case 0; when(foo(1)); end
+                          ^ Do not use unnecessary parentheses for method calls.
+        RUBY
+      end
     end
   end
 
